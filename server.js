a6c2a67bf2b181070ca70fb83cdea178017cf7c9
Jack Gallivan <jackmgallivan@gmail.com>
2021-11-07 20:29:33 -0800

    fixed syntax error

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const routes = require('./api/routes')
 const app = express()
-const PORT = process.env.port || 3000
+const PORT = 3000
 app.use(express.json())
 
 routes(app)
 
-app.listenerCount(PORT, () => {
+app.listen(PORT, () => {
 	console.log('Server started on port: ' + PORT)
 })
